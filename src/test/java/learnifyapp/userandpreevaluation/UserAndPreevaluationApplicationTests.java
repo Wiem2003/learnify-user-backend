c3cc71eca0faeecb23bb6d4bb6ff0998ebc75f48
Wiem2003 <wiem.werghemmil@esprit.tn>
2026-05-02 19:16:37 +0100

    file

--- src/test/java/learnifyapp/userandpreevaluation/UserAndPreevaluationApplicationTests.java
+++ src/test/java/learnifyapp/userandpreevaluation/UserAndPreevaluationApplicationTests.java
@@ -1,12 +1,11 @@
 package learnifyapp.userandpreevaluation;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UserAndPreevaluationApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testMain() {
+        UserAndPreevaluationApplication.main(new String[]{});
     }
 }
