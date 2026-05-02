ad01354857e71329adc5fe200726c91648b61c44
Wiem2003 <wiem.werghemmil@esprit.tn>
2026-05-02 14:22:48 +0100

    file

--- src/test/java/learnifyapp/userandpreevaluation/UserAndPreevaluationApplicationTests.java
+++ src/test/java/learnifyapp/userandpreevaluation/UserAndPreevaluationApplicationTests.java
@@ -3,7 +3,9 @@ package learnifyapp.userandpreevaluation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class UserAndPreevaluationApplicationTests {
 
     @Test
