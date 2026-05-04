fd2c0b137e262eb9056ff991db0fd62cc01917d1
Wiem2003 <wiem.werghemmil@esprit.tn>
2026-05-04 20:23:11 +0100

    Jenkinsfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
 
-  apiBase: 'http://192.168.56.10:8085/api',
-  courseApiBase: 'http://192.168.56.10:8085/api/courses',
-  apiGatewayUrl: 'http://192.168.56.10:8085',
+  apiBase: 'http://192.168.56.10:8080/api',
+  courseApiBase: 'http://192.168.56.10:8080/api/courses',
+  apiGatewayUrl: 'http://192.168.56.10:8080',
 
   oauthUserServiceUrl: 'http://192.168.56.10:8081'
 };
