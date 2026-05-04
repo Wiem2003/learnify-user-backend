477612f6713a80420de6a25c978a855e3f74509a
Wiem2003 <wiem.werghemmil@esprit.tn>
2026-05-04 19:55:52 +0100

    Jenkinsfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  apiBase: 'http://localhost:8080/api',
-  courseApiBase: 'http://localhost:8080/api/courses',
-  apiGatewayUrl: 'http://localhost:8080',
-  /** OAuth : ajouter /oauth2/** sur la gateway ou exposer l’URL réelle du user-service. */
-  oauthUserServiceUrl: 'http://localhost:8087',
+
+  apiBase: 'http://192.168.56.10:8085/api',
+  courseApiBase: 'http://192.168.56.10:8085/api/courses',
+  apiGatewayUrl: 'http://192.168.56.10:8085',
+
+  oauthUserServiceUrl: 'http://192.168.56.10:8081'
 };
